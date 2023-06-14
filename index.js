bf056af417843d348fb8f67304716ce918efd4ce
ziglewis <ziglewis48@gmail.com>
2023-06-14 15:04:59 +0100

    got all elements for Dom manipulation

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 
+  // code for live time on screen
+setInterval(myTimer, 1000);
+
+function myTimer() {
+const digitalTime = document.querySelector(".display")
+ const date = new Date();
+digitalTime.textContent = date.toLocaleTimeString() + " " + date.toLocaleDateString()
+
+; }
+
+
+
+
 // I am getting all the elements I need to manipulate the DOM
 
-prompt-display
+ let myPrompt = document.querySelector(".prompt-display")
+ let myToDoTitleInputField = document.querySelector(".To-do-title")
+ let myTaskDescriptionInputField = document.querySelector("Task-description")
+ let myTimeInputField = document.querySelector(".time-input")
+ let myDateInputField = document.querySelector(".date-input")
+ let myAddToCartBtn = document.querySelector(".Add-to-cart-btn")
+ let myViewCartBtn = document.querySelector(".View-cart-btn")
+ let myCartBtnNumber = document.querySelector(".cart-buttons-number")
+ let myCartBtnEdit = document.querySelector("cart-buttons-edit")
+ let myCartBtnDelete = document.querySelector(".cart-buttons-delete")
+ let myCartBtnStatus = document.querySelector(".cart-buttons-status")
+ let myEachTask = document.querySelectorAll(".each-task")
 
-To-do-title
 
-Task-description
+
+
+
+
